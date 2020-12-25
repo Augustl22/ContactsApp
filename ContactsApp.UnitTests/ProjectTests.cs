95374fd3bbdac76dba01d57e496116d6b41827a7
Augustl22 <Raket5420@gmail.com>
2020-12-25 18:03:50 +0700

    убрал лишние библиотеки

--- ContactsApp.UnitTests/ProjectTests.cs
+++ ContactsApp.UnitTests/ProjectTests.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using ContactsApp;
 using NUnit.Framework;
 
 namespace ContactsApp.UnitTests
